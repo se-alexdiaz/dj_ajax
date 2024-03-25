fd3bcad323b16c867f77f3e14a930114deb7b53c
Alejandro <ppenafieldiaz0856@conestogac.on.ca>
2024-03-25 17:51:36 -0400

    Finished post-save singal module

--- src/posts_proj/settings.py
+++ src/posts_proj/settings.py
@@ -121,6 +121,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
+
+
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
