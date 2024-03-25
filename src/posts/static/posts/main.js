09cba8de35e5c7d71f125ac74e39b56e7443f055
Alejandro <ppenafieldiaz0856@conestogac.on.ca>
2024-03-25 19:59:23 -0400

    Spinner module finish
    Added loading animation for posts

--- src/posts/static/posts/main.js
+++ src/posts/static/posts/main.js
@@ -1,6 +1,27 @@
-console.log('Hello from main.js')
+
+const postsBox = document.getElementById('postsBox')
+const spinnerBox = document.getElementById('spinnerBox')
+
+$.ajax({
+    type: 'GET',
+    url: 'data/',
+    success: function(response) {
+        console.log(response)
+        const data = response.data
+
+        setTimeout(() => {
+            spinnerBox.classList.add('not-visible')
+
+            data.forEach(element => {
+               postsBox.innerHTML += ` 
+               ${element.title} - <b> ${element.author} </b> <br> ${element.content} <br> <br>`
+            });
+        }, 100);
 
 
-const hw = document.getElementById('hello-world')
+    },
+    error: function(error) {
+        console.log('error', error)
+    }
 
-hw.textContent = 'Hello from main.js'
+})
