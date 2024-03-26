dc7e543dc44e15b3cbd2cd4ac64d7a847cfce71b
Alejandro <ppenafieldiaz0856@conestogac.on.ca>
2024-03-25 20:14:50 -0400

    Posts Cards module completed
    The application is now able to post individual sections for posts.

--- src/posts/static/posts/main.js
+++ src/posts/static/posts/main.js
@@ -14,7 +14,26 @@ $.ajax({
 
             data.forEach(element => {
                postsBox.innerHTML += ` 
-               ${element.title} - <b> ${element.author} </b> <br> ${element.content} <br> <br>`
+                <div class="card mb-2">
+
+                    <div class="card-body">
+                        <h5 class="card-title">${element.title}</h5>
+                        <p class="card-text">${element.content}</p>
+                    </div>
+
+                    <div class="card-footer">
+                        <div class="row">
+                            <div class="col-1">
+                                <a href="#" class="btn btn-primary">Details</a>
+                            </div>
+                            <div class="col-2">
+                                <a href="#" class="btn btn-primary">Likes</a>
+                            </div>
+                        </div>
+                    </div>
+                    
+                </div>
+               `
             });
         }, 100);
 
