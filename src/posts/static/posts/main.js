49cc9699f9d1f2dd06304cf9fe8472b4b22a8eea
Alejandro <ppenafieldiaz0856@conestogac.on.ca>
2024-03-25 22:28:27 -0400

    Loading post module finished
    The application is now coded so that at first it will only show three posts at a time and only  if the user clicks the "load more button" will more post load in 3s

--- src/posts/static/posts/main.js
+++ src/posts/static/posts/main.js
@@ -1,46 +1,71 @@
 
 const postsBox = document.getElementById('postsBox')
 const spinnerBox = document.getElementById('spinnerBox')
+const loadBtn = document.getElementById('loadBnt')
+const endBox = document.getElementById('endBox')
 
-$.ajax({
-    type: 'GET',
-    url: 'data/',
-    success: function(response) {
-        console.log(response)
-        const data = response.data
+let visible = 3
 
-        setTimeout(() => {
-            spinnerBox.classList.add('not-visible')
+const getData = () => {
 
-            data.forEach(element => {
-               postsBox.innerHTML += ` 
-                <div class="card mb-2">
-
-                    <div class="card-body">
-                        <h5 class="card-title">${element.title}</h5>
-                        <p class="card-text">${element.content}</p>
-                    </div>
-
-                    <div class="card-footer">
-                        <div class="row">
-                            <div class="col-1">
-                                <a href="#" class="btn btn-primary">Details</a>
-                            </div>
-                            <div class="col-2">
-                                <a href="#" class="btn btn-primary">Likes</a>
+    $.ajax({
+        type: 'GET',
+        url: `/data/${visible}/`,
+        success: function(response) {
+            console.log(response)
+            const data = response.data
+    
+            setTimeout(() => {
+                spinnerBox.classList.add('not-visible')
+    
+                data.forEach(element => {
+                   postsBox.innerHTML += ` 
+                    <div class="card mb-2">
+    
+                        <div class="card-body">
+                            <h5 class="card-title">${element.title}</h5>
+                            <p class="card-text">${element.content}</p>
+                        </div>
+    
+                        <div class="card-footer">
+                            <div class="row">
+                                <div class="col-1">
+                                    <a href="#" class="btn btn-primary">Details</a>
+                                </div>
+                                <div class="col-2">
+                                    <a href="#" class="btn btn-primary">Likes</a>
+                                </div>
                             </div>
                         </div>
+                        
                     </div>
-                    
-                </div>
-               `
-            });
-        }, 100);
+                   `
+                });
+            }, 100);
 
+            console.log(response.size)
+            if (response.size == 0) {
+                endBox.textContent = 'No posts added yet...'
+            }
+            else if (response.size <= visible) {
+                loadBtn.classList.add('not-visible')
+                endBox.textContent = 'No more posts to load...'
+            }
+    
+    
+        },
+        error: function(error) {
+            console.log('error', error)
+        }
+    
+    })
+}
 
-    },
-    error: function(error) {
-        console.log('error', error)
-    }
+loadBtn.addEventListener('click', () => {
+    spinnerBox.classList.remove('not-visible')
+    visible += 3
+    getData()
 
 })
+
+getData()
--- src/posts/urls.py
+++ src/posts/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from .views import (
     post_list_and_create,
-    hello_world_view,
+    
     load_post_data_view
 )
 
@@ -9,7 +9,6 @@ app_name = 'posts'
 
 urlpatterns = [
     path('', post_list_and_create, name='main-board'),
-    path('data/', load_post_data_view, name='posts-data'),
+    path('data/<int:num_posts>/', load_post_data_view, name='posts-data')
 
-    path('hello-world/', hello_world_view, name='hello-world')
 ]
