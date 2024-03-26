b3bf52ea436fd660d2fceea0335512cdd69838ca
Alejandro <ppenafieldiaz0856@conestogac.on.ca>
2024-03-26 15:16:47 -0400

    Finalized like-unlike functionality

--- src/posts/static/posts/main.js
+++ src/posts/static/posts/main.js
@@ -6,6 +6,52 @@ const endBox = document.getElementById('endBox')
 
 let visible = 3
 
+// using jQuery
+const getCookie = (name)  =>{
+    var cookieValue = null;
+    if (document.cookie && document.cookie != '') {
+        var cookies = document.cookie.split(';');
+        for (var i = 0; i < cookies.length; i++) {
+            var cookie = jQuery.trim(cookies[i]);
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) == (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+const csrftoken = getCookie('csrftoken');
+
+const likeUnlikePosts = () => {
+
+    const likeUnlikeForms =  [...document.getElementsByClassName('like-unlike-forms')]
+    likeUnlikeForms.forEach(form => form.addEventListener('submit', e=> {
+        e.preventDefault()
+
+        const clickedId = e.target.getAttribute('data-form-id')
+        const clickedBtn = document.getElementById(`like-unlike-${clickedId}`)
+
+        $.ajax({
+            type: 'POST',
+            url: "/like-unlike/",
+            data: {
+                'csrfmiddlewaretoken': csrftoken,
+                'pk': clickedId,
+            },
+            success: function(response) {
+                console.log(response)
+                clickedBtn.textContent = response.liked ? `Unlike (${response.count})` : `Like (${response.count})`
+            },
+            error: function(error) {
+                console.log('Error:', error)
+            }
+        })
+
+    }))
+}
+
 const getData = () => {
 
     $.ajax({
@@ -33,7 +79,9 @@ const getData = () => {
                                     <a href="#" class="btn btn-primary">Details</a>
                                 </div>
                                 <div class="col-2">
-                                    <a href="#" class="btn btn-primary">${element.liked ? `Unlike (${element.count}`: `Like (${element.count}`}</a>
+                                    <form class="like-unlike-forms" data-form-id="${element.id}">                                        
+                                        <button href="#" id="like-unlike-${element.id}" class="btn btn-primary">${element.liked ? `Unlike (${element.count}`: `Like (${element.count}`}</button>
+                                    </form>
                                 </div>
                             </div>
                         </div>
@@ -41,10 +89,12 @@ const getData = () => {
                     </div>
                    `
                 });
+                likeUnlikePosts()
             }, 100);
 
+
             console.log(response.size)
-            if (response.size == 0) {
+            if (response.size === 0) {
                 endBox.textContent = 'No posts added yet...'
             }
             else if (response.size <= visible) {
@@ -64,7 +114,7 @@ const getData = () => {
 loadBtn.addEventListener('click', () => {
     spinnerBox.classList.remove('not-visible')
     visible += 3
-    getData()
+    // getData()
 
 })
 
--- src/posts/urls.py
+++ src/posts/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from .views import (
     post_list_and_create,
-    
+    like_unlike_post,
     load_post_data_view
 )
 
@@ -9,6 +9,7 @@ app_name = 'posts'
 
 urlpatterns = [
     path('', post_list_and_create, name='main-board'),
+    path('like-unlike/', like_unlike_post, name='like-unlike'),
     path('data/<int:num_posts>/', load_post_data_view, name='posts-data')
 
 ]
